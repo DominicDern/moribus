f0f632b5bd2446f1eb377177a87a3223952b63d8
Dominic Dern <74207434+DominicDern@users.noreply.github.com>
2023-09-18 16:04:14 -0300

    Roll parsing and generation completed, input checking is still required but basic functionality is now complete.

---
+++ target/debug/build/libc-b0930ec71dfeb7fc/build_script_build-b0930ec71dfeb7fc.d
@@ -0,0 +1,5 @@
+/home/dominic/Documents/Code/Rust/moribus/target/debug/build/libc-b0930ec71dfeb7fc/build_script_build-b0930ec71dfeb7fc: /home/dominic/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.148/build.rs
+
+/home/dominic/Documents/Code/Rust/moribus/target/debug/build/libc-b0930ec71dfeb7fc/build_script_build-b0930ec71dfeb7fc.d: /home/dominic/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.148/build.rs
+
+/home/dominic/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.148/build.rs:
---
+++ target/debug/deps/moribus-d037e92a3dca848f.d
@@ -0,0 +1,7 @@
+/home/dominic/Documents/Code/Rust/moribus/target/debug/deps/moribus-d037e92a3dca848f: src/main.rs src/utilities/mod.rs src/utilities/roll.rs
+
+/home/dominic/Documents/Code/Rust/moribus/target/debug/deps/moribus-d037e92a3dca848f.d: src/main.rs src/utilities/mod.rs src/utilities/roll.rs
+
+src/main.rs:
+src/utilities/mod.rs:
+src/utilities/roll.rs:

f40e407b56f8e1f9165a9e548ebbdd3e2126e808
Dominic Dern <74207434+DominicDern@users.noreply.github.com>
2023-10-11 16:17:19 -0300

    Added Result return type to main for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod utilities;
 
-use crate::utilities::roll::Roll;
-
-fn main() {
-    let ur_mom = Roll::roll("3d6, 1d12+8");
-    print!("{}", ur_mom);
+fn main() -> Result<(), ()> {
+    Ok(())
 }

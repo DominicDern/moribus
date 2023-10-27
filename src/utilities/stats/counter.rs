92a026533afb5433576a621a988ade4d4919728e
Dominic Dern <74207434+DominicDern@users.noreply.github.com>
2023-10-27 14:18:02 -0300

    Worked on counter

--- src/utilities/stats/counter.rs
+++ src/utilities/stats/counter.rs
@@ -9,26 +9,31 @@ pub struct Counter<T> {
 }
 
 impl Counter<u32> {
-    fn add(&mut self, increment: u32) -> &u32 {
-        if (self.current_value + increment) > self.max {
-            self.current_value = self.max;
-        } else {
-            self.current_value = self.current_value + increment;
-        }
-        &self.current_value
-    }
-
-    fn subtract(&mut self, decrement: u32) -> &u32 {
-        self.current_value = self.current_value - decrement;
-        &self.current_value
-    }
-    
     fn current(&self) -> &u32 {
         &self.current_value
     }
 
     fn max(&self) -> &u32 {
         &self.max
+    }
+
+    fn add(&mut self, increment: u32) -> &u32 {
+        if (self.current_value + increment) > self.max {
+            self.current_value = self.max;
+        } else {
+            self.current_value = self.current_value + increment;
+        }
+        self.current()
+    }
+
+    fn subtract(&mut self, decrement: u32) -> &u32 {
+        self.current_value = self.current_value - decrement;
+        self.current()
+    }
+
+    fn max_add(&mut self, increment: u32) -> &u32 {
+        self.max = self.max + increment;
+        self.max()
     }
 
     fn is_zero(&self) -> bool {
@@ -38,6 +43,7 @@ impl Counter<u32> {
 
 impl Stat<(u32, u32)> for Counter<u32> {
     fn name(&self) -> &String {
+        let e = vec!(1, 2, 3);
         &self.name
     }
     fn value_str(&self) -> String {
